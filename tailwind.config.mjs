f6db0487d0bd719e63bc44d1994e954822957410
Bryan Muñoz <swsbmm@gmail.com>
2023-11-16 15:45:27 -0500

    second commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+			  'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+			  'gradient-conic':
+				'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			aspectRatio: {
+			  '3/2': '3 / 2'
+			},
+		  },
+	},
+	plugins: [require('daisyui')],
+}
